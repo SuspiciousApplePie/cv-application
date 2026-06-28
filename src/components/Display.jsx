e997866a45e6ab1bf80ec8a4973f02146398b94b
SuspiciousApplePie <andreiusita6@gmail.com>
2026-06-28 17:12:46 +0800

    Add new wrapper inside cv wrapper fo styling

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,14 @@
 import { wrapper } from "./constant";
-
+import "./global.css";
+import "./Display.css";
 function Display({ renderGeneral, renderEduc, renderPrac }) {
   return (
     <div className={wrapper.CV_WRAPPER}>
-      {renderGeneral()}
-      {renderEduc()}
-      {renderPrac()}
+      <div className={wrapper.PAPER}>
+        {renderGeneral()}
+        {renderEduc()}
+        {renderPrac()}
+      </div>
     </div>
   );
 }
